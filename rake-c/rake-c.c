bcbff35c6cac822fcfbfa57661f46d9944f9825c
Anfernee <anferneealviar99@hotmail.com>
2022-04-28 13:22:31 +0800

    small changes

---
+++ rake-c/rake-c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+// locate, open, parse
+
+void file_process(str_file)
+{
+
+}
+
+int main (int argc, char *argv[])
+{
+    // command-line
+    return 0; 
+}
