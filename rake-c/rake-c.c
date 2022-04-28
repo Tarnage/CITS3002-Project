9db05096b7cbd8b27eb447e0a81c6095d85a7007
Anfernee <anferneealviar99@hotmail.com>
2022-04-28 15:59:26 +0800

    rake.c - file opening and basics

--- rake-c/rake-c.c
+++ rake-c/rake-c.c
@@ -1,15 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include <string.h>
+#include <ctype.h>
 
-// locate, open, parse
+#define MAX_LINE_LENGTH 80
 
-void file_process(str_file)
+void file_process(char *file_name)
 {
+    FILE *fp = fopen(file_name, "r");
+    if(fp == NULL)
+    {
+        fprintf(stderr, "Invalid file\n");
+    }
+    else
+    {
+        char line[MAX_LINE_LENGTH] = "";
+        // READ AND PARSE FILES LINE BY LINE
+        while(fgets(line, MAX_LINE_LENGTH, fp))
+        {
+            if (line[0] == '#')
+            {
+                continue;
+            }
+
+			if (line[0] == '\t')
+			{
+				printf("action\n");
+			
+			}
+
+            if (line[0] == '\t' && line[1] == '\t') 
+            {
+				printf("Req prog\n");
+
+            }
+
+			
+        }
+    }
 
 }
 
 int main (int argc, char *argv[])
 {
-    // command-line
+    char* file_name = argv[1];
+    file_process(file_name);
+
     return 0; 
 }
