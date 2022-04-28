fdff0172ab2d012c105b7b369dea9823bf3703c1
Ethan Pui <ethanpui@gmail.com>
2022-04-28 20:28:29 +0800

    Initial setup

---
+++ rake-p/Test.py
@@ -0,0 +1,10 @@
+
+# open the sample file used
+file = open('Rakefile')
+  
+# read the content of the file opened
+content = file.readlines()
+  
+# read 10th line from the file
+print("tenth line")
+print(content[9])
