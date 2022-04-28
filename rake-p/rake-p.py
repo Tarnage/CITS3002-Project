08835f49c8c12119147f89d70b02de39e3225f4b
Ethan Pui <ethanpui@gmail.com>
2022-04-28 14:21:44 +0800

    Added Rakefile.txt

---
+++ rake-p/rake-p.py
@@ -0,0 +1,9 @@
+
+# Must find then read the Rakefile (txt file)
+# Deafult: try and open and read from Rakefile in current directory or provide files location on rake client's command line.
+
+
+#  Create an struct for hostname and port number
+
+# Create an Array-Action, Array2 - remote, Array 3 - requires
+
