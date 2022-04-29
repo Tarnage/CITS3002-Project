773f894869cb96aacfd361ed27c3aeb5a48838de
Ethan Pui <ethanpui@gmail.com>
2022-04-29 17:29:31 +0800

    Completed identification printout
    All lines are printout with accurate description

--- rake-p/rake-p.py
+++ rake-p/rake-p.py
@@ -11,30 +11,41 @@ array_actions = []
 # content = file.readlines()
 count = 0
 for line in file.readlines():
-    
+
     # print(line)
-# print("Line{}: {}".format(count, line.split()))
+    # print("Line{}: {}".format(count, line.split()))
     # Ignore lines starting with #
     if not (line.startswith('#')):
-        # print("ignore line")
+        # print("PrintAll{}: {}".format(count, line.split()))
+        count += 1
 
-        # If line doesn't start with a space
-        if  (line[0] == '\t'):
-            count += 1
+        # Remove blank lines
+        if not line.strip():
+            continue
 
-            print("Line{}: {}".format(count, line.split()))
-            # Save the action sets in arrays
-            # array_actions.append(line)
-            # print(array_actions)
+            # 1 tab
+        if (line[0] == '\t' and line[1] != '\t'):
+            print("	One Tab{}: {}".format(count, line.split()))
+
+            #  2 tabs
+        if (line[1] == '\t'):
+            print("		Two Tabs-{}: {}".format(count, line.split()))
+            # Checker and removal for empty line
+
+            # Port and Hosts
+        if(line[0] != '\t' and line[1] != '\t' and line[0] != 'a'):
+            print("Port and Host-{}: {}".format(count, line.split()))
+
+        if(line[0] != '\t' and line[1] != '\t' and line[0] == 'a'):
+            print("Actionset-{}: {}".format(count, line.split()))
 
     # print(line[9])
     # begins with one tab = actions, create array with all lines with this (add to list)
-    
+
     # if (line.startswith('\t')):
     #     # add string to array_actions
     #     # array_actions
     #     print('reached an action')
-        
 
 
 # print(file.read())
@@ -43,17 +54,9 @@ for line in file.readlines():
 #  (2. begins with one tab = actions)
 #  (3. Lines that begin with two tabs = indicate which files are required for the previous action)
 
-# Store the information from each line into scalar variables or arrays. 
+# Store the information from each line into scalar variables or arrays.
 # There is no sorting or fast-lookup required, so arrays, and arrays of arrays, will be sufficient. Distinguish between local and remote actions.
 # Could use split() to break each line into words
-
-
-
-
-
-
-
-
 
 
 # Finding all name
@@ -67,9 +70,7 @@ for line in file.readlines():
 #     return result
 
 
-
 # Additional Notes
 #  Create an struct for hostname and port number
 
 # Create an Array-Action, Array2 - remote, Array 3 - requires
-
