36f120b0f9b45a354e58e08ba4c6099c8bde8ac7
Anfernee <anferneealviar99@hotmail.com>
2022-05-09 13:23:13 +0800

    started perform_actions module

--- rake-c/rake-c.c
+++ rake-c/rake-c.c
@@ -1,18 +1,6 @@
 #include "rake-c.h"
 
-void perform_actions(ACTION_SET *action_set)
-{
-    printf("Number of sets: %d\n", num_sets);
-    for (int i = 0; i < num_sets; i++)
-    {
-        printf("Number of actions: %d\n", action_set[i].action_totals);
-        for (int j = 0; j < action_set[i].action_totals; j++)
-        {
-            
-            printf("%s\n", ACTION_DATA(i, j).command);
-        }
-    }
-}
+
 
 int main (int argc, char *argv[])
 {
--- rake-c/rake-c.h
+++ rake-c/rake-c.h
@@ -8,11 +8,11 @@
 #include "logger_c.h"
 #include "parse_c.h"
 #include "structures.h"
+#include "execution.h" 
 #define ACTION_DATA(i,j)     action_set[i].actions[j]
 
 ACTION_SET action_set[MAX_ACTIONS];
 HOST       hosts[MAX_HOSTS];
 
-extern void perform_actions(ACTION_SET *);
 
 #endif
