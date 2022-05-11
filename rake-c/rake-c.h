a564c74125e176de34f6de65285d651a49ff7901
Anfernee <anferneealviar99@hotmail.com>
2022-05-11 10:12:01 +0800

    added variables and enum for rake-c

--- rake-c/rake-c.h
+++ rake-c/rake-c.h
@@ -13,6 +13,25 @@
 
 #define ACTION_DATA(i,j)     action_set[i].actions[j]
 
+enum command {
+    CMD_ECHO,
+    CMD_ECHOREPLY,
+    CMD_QUOTE_REQUEST,
+    CMD_QUOTE_REPLY,
+    CMD_SEND_REQUIREMENTS,
+    CMD_BIN_FILE,
+    CMD_SEND_FILE,
+    CMD_SEND_SIZE,
+    CMD_SEND_NAME,
+    CMD_EXECUTE_REQ,
+    CMD_EXECUTE,
+    CMD_RETURN_STATUS,
+    CMD_RETURN_STDOUT
+    CMD_RETURN_STDERR,
+    CMD_RETURN_FILE,
+    CMD_ACK
+}
+
 ACTION_SET action_set[MAX_ACTIONS];
 HOST       hosts[MAX_HOSTS];
 
