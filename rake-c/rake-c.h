a75f7491bca9c768de947ee8d5b062f7b6158958
Tom <1991tnguyen@gmail.com>
2022-05-24 00:14:26 +0800

    added doubly linked list

--- rake-c/rake-c.h
+++ rake-c/rake-c.h
@@ -56,7 +56,65 @@ typedef struct _node
     CMD curr_req;
     ACTION *actions;
     struct _node *next;
+    struct _node *prev;
     
 } NODE;
+
+// INIT FUNCTION FOR NODES
+void create_node(NODE *new_node, int sd, char *ip, int port)
+{
+    new_node = (NODE*)malloc(sizeof(NODE));
+    new_node->sock = sd;
+    new_node->ip = ip;
+    new_node->port = port;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+}
+
+// APPEND NEWLY CREATED NODE TO HEAD
+void append_new_node(NODE *head, NODE *new_node)
+{
+    NODE *temp = head;
+    while(temp->next != NULL) temp = temp->next;
+    temp->next = new_node;
+    new_node->prev = temp;
+    free(temp);
+}
+
+// REMOVE NODE ASSOCIATED TO THE SOCK FD
+// ALSO CLOSES THE SOCK FD
+void remove_sd(NODE *head, int sd)
+{
+    NODE *temp = head;
+    while(temp->sock != sd) temp = temp->next;
+    shutdown(temp->sock, SHUT_RDWR);
+    close(temp->sock);
+    NODE *prev = temp->prev;
+    temp->next->prev = prev;
+    prev->next = temp->next;
+    free(temp);
+}
+
+void add_cost(NODE *head, int sd, int cost)
+{
+    NODE *temp = head;
+    while(temp->sock != sd) temp = temp->next;
+    temp->cost = cost;
+    free(temp);
+}
+
+void add_action(NODE *node, ACTION *act)
+{
+    node->actions = act;
+}
+
+void find_sock(NODE *head, NODE *result, int sd)
+{   
+    NODE *temp = head;
+    while (temp->sock != sd) temp = temp->next;
+    // DONT KNOW IF I HAVE DONE THIS RIGHT
+    *result = *temp;
+}
+
 
 #endif
