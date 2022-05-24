f409009af8ea3353060dceb828a7a2e3ce49276c
Tom <1991tnguyen@gmail.com>
2022-05-24 18:47:16 +0800

    error handling

--- rake-p/parse_rakefile.py
+++ rake-p/parse_rakefile.py
@@ -30,72 +30,76 @@ def read_rake(filename):
 		host (dict[str]:[int]): key: hostname value: port
 		action_sequence( list( list(Action) ) ): will contain action objects to be run in sequence
 	'''
-	with open(filename) as rake_file:
+	try:
+		with open(filename) as rake_file:
 
-		global default_port
+			global default_port
 
-		# holds current actionset of Action objects
-		# appends the list to action_sequence once all actions in a set is recorded
-		action_set = list()
+			# holds current actionset of Action objects
+			# appends the list to action_sequence once all actions in a set is recorded
+			action_set = list()
 
-		# track hostname and port number
-		hosts = dict()
+			# track hostname and port number
+			hosts = dict()
 
-		# holds all acitionsets to be executed in order
-		action_sequence = list()
-		
-		for line in rake_file:
+			# holds all acitionsets to be executed in order
+			action_sequence = list()
+			
+			for line in rake_file:
 
-			# check if line is a comment or empty line
-			if not line[0] == '#' and not line == '\n':
+				# check if line is a comment or empty line
+				if not line[0] == '#' and not line == '\n':
 
-				# check if line is for port
-				words = line.split()
-				if words[0] == 'PORT':
-					default_port = int(words[2])
-				
-				# checks if line is for hosts
-				if words[0] == "HOSTS":
-					for hostname in words[2:]:
-						host_split = hostname.split(":")
-						if len(host_split) == 1:
-							hosts[host_split[0]] = default_port
+					# check if line is for port
+					words = line.split()
+					if words[0] == 'PORT':
+						default_port = int(words[2])
+					
+					# checks if line is for hosts
+					if words[0] == "HOSTS":
+						for hostname in words[2:]:
+							host_split = hostname.split(":")
+							if len(host_split) == 1:
+								hosts[host_split[0]] = default_port
+							else:
+								hosts[host_split[0]] = int(host_split[1])
+
+					# checks single
+					if line[0] == '\t' and not line[1] == '\t':
+
+						# split by first occuernace of a '-'
+						action_split = line.split('-', 1)
+						# if len is > 1 means we have found "remote-"
+						if(len(action_split) > 1):
+							remote = action_split[0].strip() == "remote"
+							action_set.append( Action(action_split[1].strip(), remote, []) )
 						else:
-							hosts[host_split[0]] = int(host_split[1])
+							# its a action to be performed locally
+							remote = False
+							action_set.append( Action(action_split[0].strip(), remote, []) )
 
-				# checks single
-				if line[0] == '\t' and not line[1] == '\t':
+					# Checks if double tab
+					if line[0] == '\t' and line[1] == '\t':
+							last_set = action_set[-1]
+							last_set.requires = line.split()
+							
+					if not line[0] == '\t' and "actionset" in line:
+						if len(action_set) > 0:
+							action_sequence.append(action_set)
+							action_set = list()
+			
+			# append last action_set tot action_sequence
+			action_sequence.append(action_set)
 
-					# split by first occuernace of a '-'
-					action_split = line.split('-', 1)
-					# if len is > 1 means we have found "remote-"
-					if(len(action_split) > 1):
-						remote = action_split[0].strip() == "remote"
-						action_set.append( Action(action_split[1].strip(), remote, []) )
-					else:
-						# its a action to be performed locally
-						remote = False
-						action_set.append( Action(action_split[0].strip(), remote, []) )
+			#print_action_sequence(action_sequence)
+			return hosts, action_sequence
 
-				# Checks if double tab
-				if line[0] == '\t' and line[1] == '\t':
-						last_set = action_set[-1]
-						last_set.requires = line.split()
-						
-				if not line[0] == '\t' and "actionset" in line:
-					if len(action_set) > 0:
-						action_sequence.append(action_set)
-						action_set = list()
-		
-		# append last action_set tot action_sequence
-		action_sequence.append(action_set)
-
-		#print_action_sequence(action_sequence)
-		return hosts, action_sequence
+	except FileNotFoundError as err:
+		print(f"{filename} DOES NOT EXISIT..")
+		sys.exit(1)
 
 def print_action_sequence(seq):
 	''' for testing
-	
 	'''
 	counter = 1
 	for sequence in seq:
